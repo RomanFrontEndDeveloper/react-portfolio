508063c62394e41b0d8580401b68ef5d105908ef
RomanFronrEnd <romariotraveler@gmail.com>
2025-09-18 18:41:55 +0300

    create section Footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,37 @@
 import React from 'react';
 import './Footer.css';
+import { followLinks, infoLinks } from '../../data/footerData';
+import FooterLinkGroup from './FooterLinkGroup';
+
 const Footer = () => {
+	const currentYear = new Date().getFullYear();
 	return (
-		<div>
-			<h2>Footer</h2>
-		</div>
+		<footer className='footer'>
+			<div className='containeer footer-wrapper'>
+				<div className='about-group'>
+					<h2>Roman</h2>
+					<p>Frontend Developer</p>
+					<a href='#about'>About Me</a>
+				</div>
+				<div className='hr'></div>
+				<FooterLinkGroup
+					title='More'
+					links={infoLinks}
+					isSocial={false}
+				/>
+
+				<div className='hr'></div>
+				<FooterLinkGroup
+					title='Follow'
+					links={followLinks}
+					isSocial={true}
+				/>
+			</div>
+			<p className='footer-copiright'>
+				© <span className='year'>{currentYear}</span> by Coding Snow.
+				All rights reserved.
+			</p>
+		</footer>
 	);
 };
 
---
+++ src/data/footerData.js
@@ -0,0 +1,42 @@
+import {
+	FaFacebookF,
+	FaInstagram,
+	FaGithub,
+	FaTiktok,
+	FaLinkedin,
+} from 'react-icons/fa';
+
+export const infoLinks = [
+	{ id: 1, href: 'skills', label: 'Skills' },
+	{ id: 2, href: 'services', label: 'Services' },
+	{ id: 3, href: 'portfolio', label: 'Portfolio' },
+	{ id: 4, href: 'contact', label: 'Contact' },
+];
+
+export const followLinks = [
+	{
+		id: 1,
+		href: 'https://www.facebook.com/romario.trend?mibextid=wwXIfr&rdid=pEcr34F4BAM3ZHxw&share_url=https%3A%2F%2Fwww.facebook.com%2Fshare%2F1KYdhrgyPa%2F%3Fmibextid%3DwwXIfr#',
+		icon: FaFacebookF,
+	},
+	{
+		id: 2,
+		href: 'https://www.instagram.com/romario_traveler_sport/?igsh=MWhmMmV0YndheTZ4Yg%3D%3D&utm_source=qr#',
+		icon: FaInstagram,
+	},
+	{
+		id: 3,
+		href: 'https://github.com/RomanFrontEndDeveloper',
+		icon: FaGithub,
+	},
+	{
+		id: 4,
+		href: 'https://www.tiktok.com/@travelerromario234?_t=ZM-8zQAnaSeJUr&_r=1',
+		icon: FaTiktok,
+	},
+	{
+		id: 5,
+		href: 'https://www.linkedin.com/feed/?legoTrackingToken=c34ZpnFFkTBxr71PqmgCc2UMfmlOrSdjtOoZsC5gr6litOoZp6Zdr6litOoVejAVejRApnhPpnlNpl9JtmUCjAZ9l4BjjR0Zuk9OpmhOjThBpShFtOpQrClQrCBvsClHpmlPnS9LqBYOtChxs6xzrDlxr3RAinhBpShFtOoMfmVLqnhFsSZgt6lDp6BT9z0Kc3RBsCZzkT9D9zROol1Ipl9OpOoZp6Zdr6lisCsCc3RKrSBQqndLk71RrT9D9zAVejAVfmhBt7dBtn5BkCRRjD1RrT9D9DhItm5CpmgZp4BMtmZOpOpejQBkildfk3RVgD9Bp79L9DhItm5CpmgZp4BQrClJpSlP9DhKpnhKqjRAinhLr7cCt6NRompBp3RAinhRrTBxr2oOtChxs6xzrDlxr3RBrm5epmtxs2pEt7tLsCsZp4BMs64CcPATejAOfmh9rCZFsT9BtyoOdPcQe3oPcPkNfmh9tioUcmoTd3xCcC4Ooj8Je6gMoyQVpCcQbjcSc34JdzcNoz4Ue30Zp4BQu6lQrCZz',
+		icon: FaLinkedin,
+	},
+];
