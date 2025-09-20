5aba577f667452981e43c2da7c849267d17b865d
RomanFronrEnd <romariotraveler@gmail.com>
2025-09-20 15:41:16 +0300

    fix for Netlify3

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,60 +1,27 @@
 import mainImg from './main-img.png';
 import aboutImg from './about-img.png';
-// import burgerWhite from './burger-white.svg';
-// import burger from './burger.svg';
 import client1 from './client1.jpg';
 import client2 from './client2.jpg';
 import client3 from './client3.png';
 import client4 from './client4.jpg';
-// import closeBtn from './clole-btn.jpg';
-// import closeWhite from './clole-white.jpg';
-// import close from './clole.jpg';
-// import landing from './landing.jpg';
-// import mainImg2 from './main-img2.jpg';
-// import menuBtn from './menu-btn.jpg';
-// import movies from './movies.jpg';
-// import notes from './notes.jpg';
 import portfolio1 from './portfolio-1.png';
 import portfolio2 from './portfolio-2.jpg';
 import portfolio3 from './portfolio-3.jpg';
 import portfolio4 from './portfolio-4.jpg';
 import portfolio5 from './portfolio-5.jpg';
 import portfolio6 from './portfolio-6.jpg';
-// import portfolio from './portfolio.jpg';
-// import recipes from './recipes.jpg';
-// import shape from './shape.jpg';
-// import todo from './todo.jpg';
-// import weather from './weather.jpg';
-// import react from './react.svg';
 
 export {
 	mainImg,
 	aboutImg,
-	// 	burgerWhite,
-	// 	burger,
 	client1,
 	client2,
 	client3,
 	client4,
-	// 	closeBtn,
-	// 	closeWhite,
-	// 	close,
-	// 	landing,
-	// 	mainImg2,
-	// 	menuBtn,
-	// 	movies,
-	// notes,
 	portfolio1,
 	portfolio2,
 	portfolio3,
 	portfolio4,
 	portfolio5,
 	portfolio6,
-	// 	portfolio,
-	// 	recipes,
-	// 	shape,
-	// 	todo,
-	// 	weather,
-	// 	react,
-	//
 };
